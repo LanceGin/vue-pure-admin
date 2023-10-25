6ce03878ed07f86b2e682270c8bbf370b1b08fc6
LanceGin <gin.lance.inside@hotmail.com>
2023-10-26 01:07:30 +0800

    test: 单证录入

--- src/views/operation/document-check/index.vue
+++ src/views/operation/document-check/index.vue
@@ -21,6 +21,7 @@ const formRef = ref();
 const {
   form,
   loading,
+  haveRow,
   columns,
   dataList,
   pagination,
@@ -30,6 +31,8 @@ const {
   openDialog,
   handleDelete,
   // handleDatabase,
+  handleEdit,
+  handleRowDblclick,
   handleSizeChange,
   handleCurrentChange,
   handleSelectionChange
@@ -113,6 +116,13 @@ const {
           <el-option label="已完成" value="4" />
         </el-select>
       </el-form-item>
+    </el-form>
+    <el-form
+      ref="formRef"
+      :inline="true"
+      :model="form"
+      class="search-form bg-bg_color w-[99/100] pl-8 pt-[12px]"
+    >
       <el-form-item>
         <el-button
           type="primary"
@@ -135,6 +145,22 @@ const {
         <el-button :icon="useRenderIcon(Upload)" @click="resetForm(formRef)">
           导出
         </el-button>
+        <el-button
+          type="primary"
+          :icon="useRenderIcon(EditPen)"
+          @click="handleEdit()"
+          :disabled="haveRow"
+        >
+          修改
+        </el-button>
+        <el-button
+          type="danger"
+          :icon="useRenderIcon(Delete)"
+          @click="handleDelete()"
+          :disabled="haveRow"
+        >
+          删除
+        </el-button>
       </el-form-item>
     </el-form>
 
@@ -148,6 +174,7 @@ const {
           border
           align-whole="center"
           showOverflowTooltip
+          highlight-current-row
           table-layout="auto"
           :loading="loading"
           :size="size"
@@ -161,75 +188,10 @@ const {
             color: 'var(--el-text-color-primary)'
           }"
           @selection-change="handleSelectionChange"
+          @row-dblclick="handleRowDblclick"
           @page-size-change="handleSizeChange"
-          @page-current-change="handleCurrentChange"
-        >
-          <template #operation="{ row }">
-            <el-button
-              class="reset-margin"
-              link
-              type="primary"
-              :size="size"
-              :icon="useRenderIcon(EditPen)"
-              @click="openDialog('编辑', row)"
-            >
-              修改
-            </el-button>
-            <el-popconfirm
-              :title="`是否确认删除客户名称为${row.name}的这条数据`"
-              @confirm="handleDelete(row)"
-            >
-              <template #reference>
-                <el-button
-                  class="reset-margin"
-                  link
-                  type="primary"
-                  :size="size"
-                  :icon="useRenderIcon(Delete)"
-                >
-                  删除
-                </el-button>
-              </template>
-            </el-popconfirm>
-            <!-- <el-dropdown>
-              <el-button
-                class="ml-3 mt-[2px]"
-                link
-                type="primary"
-                :size="size"
-                :icon="useRenderIcon(More)"
-              />
-              <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item>
-                    <el-button
-                      :class="buttonClass"
-                      link
-                      type="primary"
-                      :size="size"
-                      :icon="useRenderIcon(Menu)"
-                      @click="handleMenu"
-                    >
-                      菜单权限
-                    </el-button>
-                  </el-dropdown-item>
-                  <el-dropdown-item>
-                    <el-button
-                      :class="buttonClass"
-                      link
-                      type="primary"
-                      :size="size"
-                      :icon="useRenderIcon(Database)"
-                      @click="handleDatabase"
-                    >
-                      数据权限
-                    </el-button>
-                  </el-dropdown-item>
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown> -->
-          </template>
-        </pure-table>
+          @current-change="handleCurrentChange"
+        />
       </template>
     </PureTableBar>
   </div>
