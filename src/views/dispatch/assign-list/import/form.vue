269da07683ac96e975d22ad1f49cc5c5536f96bd
LanceGin <gin.lance.inside@hotmail.com>
2024-06-05 22:29:56 +0800

    test: 进口&暂落派车单车号可检索

--- src/views/dispatch/assign-list/import/form.vue
+++ src/views/dispatch/assign-list/import/form.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { formRules } from "./utils/rule";
 import { FormProps } from "./utils/types";
+import { vehicleInfoList } from "@/api/vehicle";
+import type { PaginationProps } from "@pureadmin/table";
 
 const props = withDefaults(defineProps<FormProps>(), {
   formInline: () => ({
@@ -38,6 +40,54 @@ const props = withDefaults(defineProps<FormProps>(), {
   })
 });
 
+/** 分页配置 */
+const pagination = reactive<PaginationProps>({
+  pageSize: 500,
+  currentPage: 1,
+  total: 0
+});
+const form = reactive({
+  id: "",
+  car_no: "",
+  driver: "",
+  mobile: ""
+});
+
+interface CompanyItem {
+  id: string;
+  car_no: string;
+  car: string;
+}
+const loading = ref(false);
+const list = ref<CompanyItem[]>([]);
+const options = ref<CompanyItem[]>([]);
+let accData = [];
+const data = vehicleInfoList({ pagination, form });
+data.then(v => {
+  accData = v.data.list;
+  list.value = accData.map(item => {
+    return {
+      id: `${item.id}`,
+      car_no: `${item.car_no}`,
+      car: `${item.car_no}-${item.driver}-${item.territory}`
+    };
+  });
+});
+
+const remoteMethod = (query: string) => {
+  if (query) {
+    loading.value = true;
+    setTimeout(() => {
+      loading.value = false;
+      options.value = list.value.filter(item => {
+        return item.car_no.toLowerCase().includes(query.toLowerCase());
+      });
+    }, 200);
+  } else {
+    options.value = [];
+  }
+};
+
 const ruleFormRef = ref();
 const newFormInline = ref(props.formInline);
 
@@ -55,12 +105,24 @@ defineExpose({ getRef });
     :rules="formRules"
     label-width="82px"
   >
-    <el-form-item label="车牌号：" prop="car_no">
-      <el-input
+    <el-form-item label="车牌号" prop="car_no">
+      <el-select
         v-model="newFormInline.car_no"
-        clearable
-        placeholder="请输入车牌号"
-      />
+        filterable
+        remote
+        reserve-keyword
+        placeholder="输入车牌号关键字"
+        :remote-method="remoteMethod"
+        :loading="loading"
+        style="width: 240px"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.id"
+          :label="item.car"
+          :value="item.car_no"
+        />
+      </el-select>
     </el-form-item>
     <!-- <el-form-item label="拆箱门点：" prop="door">
       <el-input
