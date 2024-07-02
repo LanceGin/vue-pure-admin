433720ccbb74725a6a0418d08cf0bf8fcc72e71f
LanceGin <gin.lance.inside@hotmail.com>
2024-07-02 23:42:11 +0800

    test: 应收流向非必填

--- src/views/statics/recievable/utils/rule.ts
+++ src/views/statics/recievable/utils/rule.ts
@@ -12,8 +12,5 @@ export const formRules = reactive(<FormRules>{
   project_name: [
     { required: true, message: "项目名称为必填项", trigger: "blur" }
   ],
-  flow_direction: [
-    { required: true, message: "流向为必填项", trigger: "blur" }
-  ],
   content: [{ required: true, message: "服务内容为必填项", trigger: "blur" }]
 });
