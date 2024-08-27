1c39d769062f6f8e925db88783cfe73c4eb83279
LanceGin <gin.lance.inside@hotmail.com>
2024-08-28 01:01:15 +0800

    test: 费用申请打印

--- src/views/daily/fee-application/reim-print.vue
+++ src/views/daily/fee-application/reim-print.vue
@@ -105,123 +105,125 @@ defineExpose({ getRef });
 
 <template>
   <div class="reim_print" style="padding: 5px 30px; overflow: hidden">
-    <table
-      border="0"
-      cellpadding="1"
-      width="100%"
-      style="padding-right: 50px; padding-bottom: 5px; padding-left: 50px"
-    >
-      <tbody>
-        <tr>
-          <td><label style="display: none" /></td>
-          <td align="right">
-            编号:<label>{{ selectRows[0].fee_no }}</label>
-          </td>
-        </tr>
-        <tr>
-          <td
-            colspan="2"
-            align="center"
-            style="font-size: 16pt; font-weight: bold"
-          >
-            <label>费 用 报 销 单</label>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <table class="auditTable" width="100%">
-      <tbody>
-        <tr>
-          <td colspan="2">
-            申请部门：<label>{{ selectRows[0].is_admin }}</label>
-          </td>
-          <td rowspan="2" style="width: 100px">支付方式</td>
-          <td rowspan="2" align="center" style="width: 50px">
-            <label>{{ selectRows[0].pay_type }}</label>
-          </td>
-          <td colspan="4">
-            收款单位全称：<label>{{ selectRows[0].company_name }}</label>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            申请: <label>{{ selectRows[0].apply_by }}</label>
-            <label>{{
-              dayjs(selectRows[0].apply_time).format("YYYY-MM-DD")
-            }}</label>
-          </td>
-          <td colspan="4">
-            开户行：<label>{{ selectRows[0].bank }}</label>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            报销: <label>{{ selectRows[0].reimburse_by }}</label> <label />
-          </td>
-          <td>申请单位</td>
-          <td>
-            <label>{{ selectRows[0].apply_department }}</label>
-          </td>
-          <td colspan="4">
-            帐号：<label>{{ selectRows[0].account_no }}</label>
-          </td>
-        </tr>
-        <tr height="30">
-          <td align="center" valign="middle">费<br />用<br />内<br />容</td>
-          <td colspan="3">
-            <label>{{ selectRows[0].fee_name }}</label>
-          </td>
-          <td align="center" valign="middle" style="width: 40px">备<br />注</td>
-          <td colspan="3">
-            <label>{{ selectRows[0].remark }}</label>
-          </td>
-        </tr>
-        <tr>
-          <td>报销金额:</td>
-          <td colspan="1" style="width: 130px">
-            <label>{{ selectRows[0].reimburse_amount }}</label>
-          </td>
-          <td>合同/发票号:</td>
-          <td colspan="1"><label /></td>
-          <td colspan="2">附件张数 <label />&nbsp;张</td>
-          <td>票据类型:</td>
-          <td><label>普通发票</label></td>
-        </tr>
-        <tr>
-          <td>大写金额:</td>
-          <td colspan="5">
-            <label>{{ getChinesePrice(selectRows[0].apply_amount) }}</label>
-          </td>
-          <td>税率:</td>
-          <td><label /><label>%</label></td>
-        </tr>
-        <tr height="50">
-          <td colspan="2">收款人:</td>
-          <td colspan="2">
-            审核:
-            <!-- <img
-              border="0"
-              src="http://www.howhan-e.com/upload/8058132213new_zhoujia.jpg"
-              style="height: 40px; vertical-align: middle"
-            /> -->
-          </td>
-          <td colspan="2">批准:</td>
-          <td colspan="2">记账:</td>
-        </tr>
-        <tr>
-          <td colspan="2">日期:</td>
-          <td colspan="2">
-            日期:
-            <!-- <label>{{
-              dayjs(selectRows[0].audit_time).format("YYYY-MM-DD")
-            }}</label> -->
-          </td>
-          <td colspan="2">日期:<label /></td>
-          <td colspan="2">日期:</td>
-        </tr>
-      </tbody>
-    </table>
-    <div align="center" style="margin-top: 10px" />
+    <div v-for="item in selectRows" :key="item" style="height: 500px">
+      <table
+        border="0"
+        cellpadding="1"
+        width="100%"
+        style="padding-right: 50px; padding-bottom: 5px; padding-left: 50px"
+      >
+        <tbody>
+          <tr>
+            <td><label style="display: none" /></td>
+            <td align="right">
+              编号:<label>{{ item.fee_no }}</label>
+            </td>
+          </tr>
+          <tr>
+            <td
+              colspan="2"
+              align="center"
+              style="font-size: 16pt; font-weight: bold"
+            >
+              <label>费 用 报 销 单</label>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="auditTable" width="100%">
+        <tbody>
+          <tr>
+            <td colspan="2">
+              申请部门：<label>{{ item.is_admin }}</label>
+            </td>
+            <td rowspan="2" style="width: 100px">支付方式</td>
+            <td rowspan="2" align="center" style="width: 100px">
+              <label>{{ item.pay_type }}</label>
+            </td>
+            <td colspan="4">
+              收款单位全称：<label>{{ item.company_name }}</label>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              申请: <label>{{ item.apply_by }}</label>
+              <label>{{ dayjs(item.apply_time).format("YYYY-MM-DD") }}</label>
+            </td>
+            <td colspan="4">
+              开户行：<label>{{ item.bank }}</label>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              报销: <label>{{ item.reimburse_by }}</label> <label />
+            </td>
+            <td>申请单位</td>
+            <td align="center">
+              <label>{{ item.apply_department }}</label>
+            </td>
+            <td colspan="4">
+              帐号：<label>{{ item.account_no }}</label>
+            </td>
+          </tr>
+          <tr height="30">
+            <td align="center" valign="middle">费<br />用<br />内<br />容</td>
+            <td colspan="3">
+              <label>{{ item.fee_name }}</label>
+            </td>
+            <td align="center" valign="middle" style="width: 40px">
+              备<br />注
+            </td>
+            <td colspan="3">
+              <label>{{ item.remark }}</label>
+            </td>
+          </tr>
+          <tr>
+            <td>报销金额:</td>
+            <td colspan="1" style="width: 130px">
+              <label>{{ item.reimburse_amount }}</label>
+            </td>
+            <td>合同/发票号:</td>
+            <td colspan="1"><label /></td>
+            <td colspan="2">附件张数 <label />&nbsp;张</td>
+            <td>票据类型:</td>
+            <td><label>普通发票</label></td>
+          </tr>
+          <tr>
+            <td>大写金额:</td>
+            <td colspan="5">
+              <label>{{ getChinesePrice(item.apply_amount) }}</label>
+            </td>
+            <td>税率:</td>
+            <td><label /><label>%</label></td>
+          </tr>
+          <tr height="50">
+            <td colspan="2">收款人:</td>
+            <td colspan="2">
+              审核:
+              <!-- <img
+                border="0"
+                src="http://www.howhan-e.com/upload/8058132213new_zhoujia.jpg"
+                style="height: 40px; vertical-align: middle"
+              /> -->
+            </td>
+            <td colspan="2">批准:</td>
+            <td colspan="2">记账:</td>
+          </tr>
+          <tr>
+            <td colspan="2">日期:</td>
+            <td colspan="2">
+              日期:
+              <!-- <label>{{
+                dayjs(selectRows[0].audit_time).format("YYYY-MM-DD")
+              }}</label> -->
+            </td>
+            <td colspan="2">日期:<label /></td>
+            <td colspan="2">日期:</td>
+          </tr>
+        </tbody>
+      </table>
+      <div align="center" style="margin-top: 10px" />
+    </div>
   </div>
 </template>
 
